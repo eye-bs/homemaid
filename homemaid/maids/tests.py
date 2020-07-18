efe0c3d5653322acb2b5ff24227c49bfaf70db0a
eye-bs <eye@odds.team>
2020-07-18 14:09:32 +0700

    Add maids model 🦊

--- homemaid/maids/tests.py
+++ homemaid/maids/tests.py
@@ -1,3 +1,26 @@
+from datetime import date
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Maid
+
+
+class TestMaid(TestCase):
+    def test_model_should_have_defind_fields(self):
+        # Django ORM
+        # Given
+        Maid.objects.create(
+            name='BB',
+            birthdate=date(1998,4,29),
+            description='Super Maid of the Year',
+            certificate='Best Maid 2012',
+            salary=3000
+        )
+        # When
+        maid = Maid.objects.last()
+        # Then
+        self.assertEqual(maid.name, 'BB')
+        self.assertEqual(maid.birthdate, date(1998,4,29))
+        self.assertEqual(maid.description, 'Super Maid of the Year')
+        self.assertEqual(maid.certificate, 'Best Maid 2012')
+        self.assertEqual(maid.salary, 3000)
